52714b03154a3a8d3ae7984081d0a049b2ad9993
Justin Lampe <xC0dex@users.noreply.github.com>
2026-04-10 20:00:59 +0200

    docs: generate xml comments

--- src/DecoratR.Abstractions/IRequestHandler.cs
+++ src/DecoratR.Abstractions/IRequestHandler.cs
@@ -6,5 +6,11 @@ namespace DecoratR;
 public interface IRequestHandler<in TRequest, TResponse>
     where TRequest : IRequest
 {
+    /// <summary>
+    /// Asynchronously handles the specified <paramref name="request"/> and returns a response of type <typeparamref name="TResponse"/>.
+    /// </summary>
+    /// <param name="request">The request to handle.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="ValueTask{TResult}"/> representing the asynchronous operation, containing the response.</returns>
     ValueTask<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
 }
